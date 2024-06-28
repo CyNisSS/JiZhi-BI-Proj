661904bfbb8cf526689b38ea79126d7ac6161779
yangty <yang1071567863@126.com>
2024-06-29 01:26:26 +0800

    使用mybatis X生成CRUD--OK

---
+++ YangtyBI-backend/src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author cynisss
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2024-06-29 00:53:57
+*/
+public interface ChartService extends IService<Chart> {
+
+}
